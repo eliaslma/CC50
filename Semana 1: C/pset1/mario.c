5e31a28319d17672888a998e1a44a30d2de9febc
Elias Lima <sci.eliaslima@gmail.com>
2022-07-13 19:34:37 -0300

    pset1: solution for exercise 1 (mario - easy version)

---
+++ Semana 1: C/pset1/mario.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int tamanho, aux;
+
+    do{
+        printf("Digite o tamanho da pirâmide: ");
+        scanf("%d",&tamanho);
+    }
+    while(tamanho < 1 || tamanho > 8 );
+
+    aux = tamanho;
+    for(int x = 0; x < tamanho; x++){
+        
+        for(int z = 0; z < aux-1; z++ ){
+                printf(" ");
+            }
+
+        for(int y = 0; y < x+1; y++){
+            printf("#");
+        }
+        aux--;    
+        printf("\n");
+    }
+
+}
